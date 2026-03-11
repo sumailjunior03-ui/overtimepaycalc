d91a7f81ac8b1a7ce9a7c623a4367b55527fffb7
sumailjunior03-ui <sumailjunior03@gmail.com>
2026-03-11 21:54:37 +0500

    updated

--- network.js
+++ network.js
@@ -5,18 +5,7 @@
 window.FORBIDDEN_DOMAINS = ["tokentodollarmargin.com"];
 
 window.CALC_HQ_NETWORK = [
-  { domain: "calc-hq.com",             name: "Calc HQ",                 url: "https://calc-hq.com/",               live: true },
-  { domain: "bizdaychecker.com",       name: "BizDayChecker",           url: "https://bizdaychecker.com/",         live: true },
-  { domain: "bankcutoffchecker.com",   name: "BankCutoffChecker",       url: "https://bankcutoffchecker.com/",     live: true },
-  { domain: "salaryvsinflation.com",   name: "SalaryVsInflation",       url: "https://salaryvsinflation.com/",     live: true },
-  { domain: "hourly2salarycalc.com",   name: "Hourly2SalaryCalc",       url: "https://hourly2salarycalc.com/",     live: true },
-  { domain: "payrolldatechecker.com",  name: "PayrollDateChecker",      url: "https://payrolldatechecker.com/",    live: true },
-  { domain: "1099vsw2calc.com",        name: "1099vsW2Calc",            url: "https://1099vsw2calc.com/",          live: true },
-  { domain: "freelanceincomecalc.com", name: "FreelanceIncomeCalc",     url: "https://freelanceincomecalc.com/",   live: true },
-  { domain: "quarterlytaxcalc.com",    name: "QuarterlyTaxCalc",        url: "https://quarterlytaxcalc.com/",      live: true },
-  { domain: "totalcompcalc.com",       name: "TotalCompCalc",           url: "https://totalcompcalc.com/",         live: true },
-  { domain: "overtimepaycalc.com",     name: "OvertimePayCalc",         url: "https://overtimepaycalc.com/",       live: true },
-  { domain: "aftertaxsalarycalc.com",  name: "AfterTaxSalaryCalc",      url: "https://aftertaxsalarycalc.com/",    live: true }
+  { domain: "calc-hq.com", name: "Calc-HQ", url: "https://calc-hq.com/", live: true }
 ];
 
 (function validateNetwork() {
